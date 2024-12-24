a51506acd5e45be06b2a75e3c1a947d4a3ff1fbd
venushaamarathunga <avenusha95@gmail.com>
2024-12-24 14:43:04 +0200

    add new animation

--- script.js
+++ script.js
@@ -12,25 +12,30 @@ function playBtnClick() {
   nameStrap.style.visibility = "visible";
   nameStrap.style.backgroundColor = "#982208";
   nameStrap.style.color = "white";
+  nameStrap.style.position = "relative";
 
   animation = anime({
     targets: "#name-strap",
     easing: "easeInOutQuint",
-    translateX: [-400, 0],
-    width: [0, 400],
+    // translateX: ["0%", "40%"],
+    width: ["0%", "40%"],
     duration: 2000,
   });
 }
 
 function stopBtnClick() {
+  if (animation) animation.pause(); // Pause any ongoing animation
+
   animation = anime({
     targets: "#name-strap",
-    easing: "easeOutCirc",
-    width: [0, 400],
-    translateX: -[-400, 0],
-    duration: 2000,
+    easing: "easeInOutQuint",
+    width: ["40%", "0%"], // Shrink width
+    translateX: ["-40%", "-10%"], // Shift position to create a left-to-right effect
+    duration: 3000,
     complete: function () {
       nameStrap.style.visibility = "hidden";
+      nameStrap.style.left = "-20px";
+      nameStrap.style.transform = "translateX(0)"; // Reset translateX after animation
     },
   });
 }
